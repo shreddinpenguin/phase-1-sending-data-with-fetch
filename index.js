6205706b5bb7eff99dac761d14805a279b435ba9
Taylor Riley <t_riley13@icloud.com>
2023-09-19 23:00:51 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+let formData = {
+    name: 'Steve',
+    email: 'steve@steve.com'
+}
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(res => res.json())
+    .then(obj => {
+        document.body.innerHTML = obj["id"]
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+        console.log(error.message)
+    })
+}
